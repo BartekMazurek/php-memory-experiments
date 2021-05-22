81ef5ab1f7c32f275d0afe484abc80d4a9803b0d
BartekMazurek <bartlomiejmazurek9@gmail.com>
2021-05-22 13:43:27 +0200

    Reorganized index file

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
 
-define('MEMORY_LIMIT_120M_IN_BYTES', 125829120);
-
-$h = fopen("test.csv", "r");
-$data = fgetcsv($h, 1000, ",");
-
-$fileLine = 0;
-while (($data = fgetcsv($h, 1000, ",")) !== FALSE)
-{
-    if (memory_get_usage() >= MEMORY_LIMIT_120M_IN_BYTES) {
-        echo "Memory limit will be probably exhausted. Script execution terminated" . PHP_EOL;
-        break;
-    }
-
-    $dataArray[] = $data;
-    echo $fileLine . ' | '. memory_get_usage() . PHP_EOL;
-    $fileLine++;
-}
-
-fclose($h);
+//require_once 'first.php';
+require_once 'second.php';
